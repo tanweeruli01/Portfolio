3ca057e1297e018ebcc982d9a609dcc2f2f32149
tanweeruli01 <tanweeruli01@gmail.com>
2024-02-22 09:44:05 +0000

    comments and functions for the menu opening/closing

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname){
     for(tablink of tablinks){
-        tablink.classList.remove("active-link");
+        tablink.classList.remove("active-link"); //experiences
     }
     for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
+        tabcontent.classList.remove("active-tab"); //skills
     }
     event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+    document.getElementById(tabname).classList.add("active-tab"); 
 }
 
+var sideMenu = document.getElementById("sideMenu")
+
+function openMenu(){
+    sideMenu.style.right = "0"; //this will open the menu bar
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-200px"; //this will close the menu bar
+}
